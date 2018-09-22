c60af85a66c559c4ebd44aa94fb7c9b95ed3559c
dingzhexue <dingzhexue@126.com>
2018-09-22 08:21:41 -0700

    Changed the color of Star rate

---
+++ PACTGO/ViewControllers/ConfirmationViewController.swift
@@ -0,0 +1,78 @@
+//
+//  ConfirmationViewController.swift
+//  PACTGO
+//
+//  Created by admin on 9/21/18.
+//  Copyright © 2018 PACTGO. All rights reserved.
+//
+
+import UIKit
+
+class ConfirmationViewController: UIViewController {
+    let appDelegate = UIApplication.shared.delegate as! AppDelegate
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        initUI()
+        navigationController?.navigationBar.isHidden = false
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        navigationController?.navigationBar.isHidden = false
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func initUI(){
+        title = "Confirmation"
+        let rightBtn = UIBarButtonItem(image: #imageLiteral(resourceName: "menu").withRenderingMode(.alwaysOriginal),
+                                       style: .plain,
+                                       target: self,
+                                       action: #selector(btnMenuTapped))
+        let leftBtn = UIBarButtonItem(image: #imageLiteral(resourceName: "back-arrow").withRenderingMode(.alwaysOriginal),
+                                      style: .plain,
+                                      target: self,
+                                      action: #selector(btnBackTapped))
+        navigationItem.titleView?.tintColor = UIColor.white
+        navigationItem.leftBarButtonItem = leftBtn
+        navigationItem.rightBarButtonItem = rightBtn
+        navigationItem.rightBarButtonItem?.tintColor = UIColor.init(red: 51 / 255,
+                                                                    green: 192 / 255,
+                                                                    blue: 174 / 255,
+                                                                    alpha: 1.0)
+        navigationController?.navigationBar.titleTextAttributes = [NSAttributedStringKey.foregroundColor: UIColor.white]
+    }
+    
+    // MARK: - Navigation button actions
+    @objc func btnMenuTapped(){
+        sideMenuController?.revealMenu()
+    }
+    
+    @objc func btnBackTapped(){
+        navigationController?.popViewController(animated: true)
+    }
+    
+    // MARK: - UIButton actions
+    @IBAction func btnProceedTapped(_ sender: Any) {
+//        self.navigationController?.pushViewController(appDelegate.tabController!, animated: true)
+    }
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
